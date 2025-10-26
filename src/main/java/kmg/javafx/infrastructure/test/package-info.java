428509516d807562e7382f032ec8f81d2aa8ed98
KenichiroArai <sammyleejunior@gmail.com>
2025-10-26 23:17:53 +0900

    #5 package-info.javaを作成した。
    (Docsを作成する #5)

---
+++ src/main/java/kmg/javafx/infrastructure/test/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * KMG JavaFXインフラストラクチャ層テスト層のパッケージ<br>
+ * <p>
+ * JavaFXのインフラストラクチャ層のテスト支援機能を格納するパッケージです。<br>
+ * JUnitテストの抽象クラスやテストユーティリティが実装されています。
+ * </p>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+package kmg.javafx.infrastructure.test;
